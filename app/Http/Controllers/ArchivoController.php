2b61df4fc2550b95cd4a07c72c3d767c78900728
Bryan Nuñez Alvarado <bryan.nunez2941@alumnos.udg>
2022-05-13 19:32:22 -0500

    afinando detalles del codigo

--- app/Http/Controllers/ArchivoController.php
+++ app/Http/Controllers/ArchivoController.php
@@ -5,17 +5,20 @@ use App\Models\Archivo;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class ArchivoController extends Controller
 {
     public function store(Request $request)
     {
+        $request->merge(['user_id'=>Auth::id()]);
+        //dd($request);
         foreach($request->archivos as $archivo){
             if($archivo->isValid()){
                 $nombre_hash = $archivo->store('user');
 
                 $registroArchivo = new Archivo();
-                $registroArchivo->user_id='1';
+                $registroArchivo->user_id=$request->user_id;
                 $registroArchivo->nombre = $archivo->getClientOriginalName();
                 $registroArchivo->nombre_hash = $nombre_hash;
                 $registroArchivo->mime = $archivo->getClientMimeType();
